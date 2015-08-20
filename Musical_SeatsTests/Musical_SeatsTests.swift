8eb8ba32f22d411cf416295119c4f9a297cb0a5a
SteveYTan <Leca@steves-macbook-pro.local>
2015-08-19 19:51:39 -0700

    Initial Commit

---
+++ Musical_SeatsTests/Musical_SeatsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Musical_SeatsTests.swift
+//  Musical_SeatsTests
+//
+//  Created by Steve T on 8/19/15.
+//  Copyright © 2015 Steve T. All rights reserved.
+//
+
+import XCTest
+@testable import Musical_Seats
+
+class Musical_SeatsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
